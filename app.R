11bbd1d08f3035ec720ebf751d94f002b7f86c7f
RuchikaNanwani <101907773+RuchikaNanwani@users.noreply.github.com>
2022-09-22 10:55:10 +0100

    Updated app.R
    added initial changes
    fixed bugs, added feature to link with select box.

--- app.R
+++ app.R
@@ -8,27 +8,38 @@ library(leaflet)
 library(sf)
 library(IMD)
 library(geographr)
+library(httr)
+library(readxl)
+library(tidyverse)
 
+# changed table to boundaries_ltla19
 # ---- Prepare data ----
 # Join the English Local Authortiy District IMD data set (exported from IMD) to 
-# the corresponding boundary (shape) file (exported from geograhr)
-imd_with_boundaries <-
-  boundaries_lad |>
-  right_join(imd_england_lad)
+# the corresponding boundary (shape) file (exported from geographr)
+
+
+df <-data.frame(imd_with_boundaries <- imd_england_lad %>% right_join( boundaries_ltla19, 
+                                                                       by=c('lad_code'='ltla19_code') ) ) 
+
+colnames(df) <- c("lad_code","Score","Proportion","Extent","Income_Score","Income_Proportion","Employment_Score","Employment_Proportion","Education_Score","Education_Proportion","Health_Score","Health_Proportion","Crime_Score","Crime_Proportion","Housing_and_Access_Score","Housing_and_Access_Proportion","Environment_Score","Environment_Proportion","co19","co20")
+todrop <- df$lad_code
+names(todrop) <- df$co19
+lad_name <- imd_with_boundaries$ltla19_name
+lad_code <- imd_with_boundaries$ltla19_code
 
 # ---- UI ----
 ui <-
   fluidPage(
-
+    
     # - Set CSS -
     includeCSS("www/styles.css"),
-
+    
     # - Title -
     fluidRow(
       align = "center",
       titlePanel("IMD Explorer")
     ),
-
+    
     # - Select Box -
     fluidRow(
       column(
@@ -36,8 +47,10 @@ ui <-
         align = "center",
         selectizeInput(
           "selectbox",
+          todrop,
+          multiple=FALSE,
           label = NULL,
-          choices = sort(imd_with_boundaries$lad_name),
+          choices = todrop,
           options = list(
             placeholder = "Select a Local Authority",
             onInitialize = I('function() { this.setValue(""); }')
@@ -45,77 +58,99 @@ ui <-
         )
       )
     ),
-
+    
     # - Map & Plot -
     fluidRow(
-
+      
       # - Map -
       column(
         width = 6,
         align = "center",
         leafletOutput("map", height = 600)
       ),
-
+      
       # - Table -
       column(
         width = 6,
         align = "center",
-        tableOutput("imdTable")
+        tableOutput("Table")
       )
     )
   )
 
-# ---- Server ----
+# ----Server----
 server <-
-  function(input, output, session) {
-
+  function(input, output, session) 
+  {
+    
     # - Track selections -
     # Track which map polygons the user has clicked on
     selected_polygon <- reactiveVal("E06000001")
-
-    observeEvent(input$map_shape_click, {
+    # Track which select id the user has clicked on
+    selected_id <- reactiveVal("E06000001")
+    
+    # function to call in observe
+    
+    input_some_selection <- reactive({
+      input$selectbox
+    })
+    
+    # Reactive function "input_some_selection" is called in observe event of select
+    
+    observeEvent(input_some_selection(),{
+      input$selectbox |>
+        selected_id()
+    })
+    
+    # Observe event of Map click
+    
+    observeEvent(input$map_shape_click,{
       input$map_shape_click$id |>
-      selected_polygon()
-    )
-
+        selected_polygon() 
+    })
+    
+    
     # - Map -
     output$map <-
       renderLeaflet({
-        leaflet() |>
-        setView(lat = 52.75, lng = -2.0, zoom = 6) |>
-        addProviderTiles(providers$CartoDB.Positron) |>
-        addPolygons(
-          data = imd_with_boundaries,
-          layerId = ~lad_code,
-          weight = 0.7,
-          opacity = 0.5,
-          # color = "#bf4aee",
-          dashArray = "0.1",
-          fillOpacity = 0.4,
-          highlight = highlightOptions(
-            weight = 5,
-            color = "#666",
-            dashArray = "",
-            fillOpacity = 0.7,
-            bringToFront = TRUE
-          ),
-          label = imd_with_boundaries$lad_name
-        )
+        leaflet()  %>% 
+          setView(lat = 52.75, lng = -2.0, zoom = 6) |>
+          addProviderTiles(providers$CartoDB.Positron) |>
+          addPolygons(data = boundaries_ltla19,
+                      layerId = ~ltla19_code,
+                      weight = 0.7,
+                      opacity = 0.5,
+                      dashArray = "0.1",
+                      fillOpacity = 0.4,
+                      highlight = highlightOptions(
+                        weight = 5,
+                        color = "#666",
+                        dashArray = "",
+                        fillOpacity = 0.7,
+                        bringToFront = TRUE
+                      ) ,
+                      label = imd_with_boundaries$ltla19_name
+          )
       })
-
-    # - Table -
+    
+    #- Table -
     output$Table <-
-      renderTable(
+      renderTable({
         imd_england_lad |>
-        filter(lad_code == selected_polygon()) |>
-        pivot_longer(
-          cols = !lad_code,
-          names_to = "Variable",
-          values_to = "Value"
-        ) |>
-        select(-lad_code)
-      )
+          filter((lad_code == selected_id()) | (lad_code==selected_polygon())) |>
+          pivot_longer(
+            cols = !lad_code,
+            names_to = "Variable",
+            values_to = "Value"
+          ) |>
+          select(-lad_code) 
+        
+      })
+    
+    
+    
   }
 
 # ---- Run App ----
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui,  server = server )
+
